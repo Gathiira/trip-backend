9888981cd578450f0c658d25dffb3682108fc283
NdiranguMartin <wanjirumartin88@gmail.com>
2020-09-26 20:15:36 +0300

    deploy

--- smokinace/wsgi.py
+++ smokinace/wsgi.py
@@ -10,11 +10,8 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
-
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smokinace.settings')
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
 
