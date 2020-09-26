b521ff360323d39ca8a4ab8858088eb5dd5009cd
NdiranguMartin <wanjirumartin88@gmail.com>
2020-09-26 20:27:56 +0300

    deploy

--- smokinace/settings.py
+++ smokinace/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-c1cg^-j%2l-e_-(0+ey030&yvz@^k$x@%w0pwgm#of%)qrqe+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['smokin-ace.herokuapp.com/','herokuapp.com']
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
