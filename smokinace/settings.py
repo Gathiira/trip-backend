cd24a2b221f3d36468e2212c105c4e35098ca2d7
Gathiira <mwangyjose@gmail.com>
2020-11-04 11:19:11 +0300

    debug true

--- smokinace/settings.py
+++ smokinace/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-c1cg^-j%2l-e_-(0+ey030&yvz@^k$x@%w0pwgm#of%)qrqe+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com']
 # ALLOWED_HOSTS = []
